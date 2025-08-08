a66172628ce7370604b9f23989c233baa43b9a5d
CMCat <1624498190@qq.com>
2025-08-08 10:27:11 +0800

    removed shitcode

--- src/main/java/tech/ccat/debugger/DebugStatProvider.java
+++ src/main/java/tech/ccat/debugger/DebugStatProvider.java
@@ -5,6 +5,8 @@ import org.jetbrains.annotations.NotNull;
 import tech.ccat.kstats.api.StatProvider;
 import tech.ccat.kstats.model.PlayerStat;
 import tech.ccat.kstats.model.StatType;
+
+import java.util.Collections;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -25,18 +27,19 @@ public class DebugStatProvider implements StatProvider {
     @Override
     public PlayerStat provideStats(Player player) {
         Map<StatType, Double> adjustments = debugValues.get(player.getUniqueId());
+        final Map<StatType, Double> stats = adjustments != null ? adjustments : Collections.emptyMap();
 
         return new PlayerStat(
-                (adjustments != null ? adjustments.getOrDefault(StatType.HEALTH, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.DEFENSE, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.STRENGTH, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.SPEED, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.CRIT_CHANCE, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.CRIT_DAMAGE, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.WISDOM, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.BASE_DAMAGE, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.DAMAGE_MULTIPLIER, 0.0) : 0.0),
-                (adjustments != null ? adjustments.getOrDefault(StatType.HEALING, 0.0) : 0.0)
+                stats.getOrDefault(StatType.HEALTH, 0.0),
+                stats.getOrDefault(StatType.DEFENSE, 0.0),
+                stats.getOrDefault(StatType.STRENGTH, 0.0),
+                stats.getOrDefault(StatType.SPEED, 0.0),
+                stats.getOrDefault(StatType.CRIT_CHANCE, 0.0),
+                stats.getOrDefault(StatType.CRIT_DAMAGE, 0.0),
+                stats.getOrDefault(StatType.WISDOM, 0.0),
+                stats.getOrDefault(StatType.BASE_DAMAGE, 0.0),
+                stats.getOrDefault(StatType.DAMAGE_MULTIPLIER, 0.0),
+                stats.getOrDefault(StatType.HEALING, 0.0)
         );
     }
 }
